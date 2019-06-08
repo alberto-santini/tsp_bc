c55247ec10222e468480b714a7a75a2df46ab0fa
Alberto Santini <santini.alberto@gmail.com>
2019-06-08 16:18:49 +0200

    Fixed instances path in cluster script

--- var/create_cluster_scripts.rb
+++ var/create_cluster_scripts.rb
@@ -12,25 +12,23 @@ FileUtils.mkdir_p('cluster-scripts')
 
 puts "Generating launchers for instance in #{instdir}..."
 
-Dir.glob(File.join(instdir, 'stsp-*')) do |folder|
-  Dir.glob(File.join(folder, '*.tsp')) do |instance|
-    inst_name = File.basename(instance, File.extname(instance))
-    out_file = "#{enum_k}-#{inst_name}.csv"
+Dir.glob(File.join(instdir, '*.tsp')) do |instance|
+  inst_name = File.basename(instance, File.extname(instance))
+  out_file = "#{enum_k}-#{inst_name}.csv"
 
-    script = <<~EOF
-    #!/bin/bash
-    #SBATCH --partition=normal
-    #SBATCH --time=3:00:00
-    #SBATCH --nodes=1
-    #SBATCH --ntasks-per-node=1
-    #SBATCH --cpus-per-task=1
-    #SBATCH --mem-per-cpu=4096
-    #SBATCH -o #{enum_k}-#{inst_name}.txt
-    #SBATCH -e err-#{enum_k}-#{inst_name}.txt
+  script = <<~EOF
+  #!/bin/bash
+  #SBATCH --partition=normal
+  #SBATCH --time=3:00:00
+  #SBATCH --nodes=1
+  #SBATCH --ntasks-per-node=1
+  #SBATCH --cpus-per-task=1
+  #SBATCH --mem-per-cpu=4096
+  #SBATCH -o #{enum_k}-#{inst_name}.txt
+  #SBATCH -e err-#{enum_k}-#{inst_name}.txt
 
-    PATH=#{binfdir} LD_LIBRARY_PATH=#{libsdir} #{File.join(execdir, 'tsp_bc')} #{instance} #{enum_k} #{out_file}
-    EOF
+  PATH=#{binfdir} LD_LIBRARY_PATH=#{libsdir} #{File.join(execdir, 'tsp_bc')} #{instance} #{enum_k} #{out_file}
+  EOF
 
-    File.write(File.join('cluster-scripts', "launch-#{enum_k}-#{inst_name}.sh"), script)
-  end
+  File.write(File.join('cluster-scripts', "launch-#{enum_k}-#{inst_name}.sh"), script)
 end
