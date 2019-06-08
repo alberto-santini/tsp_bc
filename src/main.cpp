d45b5f08b25b3f3faf71451b82476d3bd9c77a5b
Alberto Santini <santini.alberto@gmail.com>
2019-06-08 16:12:46 +0200

    Added custom output file

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
+#include <filesystem>
 
 #include "CplexSolver.h"
 
 int main(int, char** argv) {
     using namespace tsp_bc;
 
-    CplexSolver solver{argv[1]};
+    // --- 1st argument: instance file ---
+    const auto inst = std::filesystem::path{argv[1]};
 
+    if(!std::filesystem::exists(inst)) {
+        std::cerr << "Instance not found: " << inst << "\n";
+        return 1;
+    }
+
+    // --- 2nd parameter: enumeration k ---
     std::stringstream ss;
     ss << argv[2];
 
@@ -17,18 +26,24 @@ int main(int, char** argv) {
         return 1;
     }
 
+    // --- 3rd parameter: output file ---
+    auto ofs = std::ofstream{argv[3]};
+
+    // --- Solve! ---
+    const auto solver = CplexSolver{inst};
     const auto solution = solver.solve(subtour_enumeration_k);
 
-    std::cout << argv[1] << ","
-              << subtour_enumeration_k << ","
-              << solution.instance_size << ","
-              << solution.num_rows << ","
-              << solution.model_creation_time_sec << ","
-              << solution.root_node_time_sec << ","
-              << solution.solver_time_sec << ","
-              << solution.obj_value << ","
-              << solution.root_node_gap << ","
-              << solution.gap << "\n";
+    // --- Print output ---
+    ofs << inst.stem().string() << ","
+        << subtour_enumeration_k << ","
+        << solution.instance_size << ","
+        << solution.num_rows << ","
+        << solution.model_creation_time_sec << ","
+        << solution.root_node_time_sec << ","
+        << solution.solver_time_sec << ","
+        << solution.obj_value << ","
+        << solution.root_node_gap << ","
+        << solution.gap << "\n";
 
     return 0;
 }
